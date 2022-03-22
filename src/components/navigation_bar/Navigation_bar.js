bc0b3e079717f1327bed48f21c2d2c2caa40e250
Pulitus <kuba.pulik@gmail.com>
2022-03-22 16:01:20 +0100

    css nav_bar

--- src/components/navigation_bar/Navigation_bar.js
+++ src/components/navigation_bar/Navigation_bar.js
@@ -6,23 +6,20 @@ const Navigation_bar = () => {
     const {t, i18n} = useTranslation();
 
     return (
-<<<<<<< HEAD
         <nav id="nav">
             <div id="logo">
                 
-=======
-        <nav>
-            <div id="logo">
-                <img src="src\components\navigation_bar\logo.png" alt="OurLogo" width="5%" height="5%"/>
->>>>>>> 73b98812dbe7bf6f2f23e8dae30411e336c6d593
+
+        
+            
             </div>
             <ul>
-                <li><p>{t("nav.hotel")}</p></li>
-                <li><p>{t("nav.price")}</p></li>
-                <li><p>{t("nav.restaurant")}</p></li>
+                <li><p>{t("nav.contact")}</p></li>
                 <li><p>{t("nav.relax")}</p></li>
                 <li><p>{t("nav.attractions")}</p></li>
-                <li><p>{t("nav.contact")}</p></li>
+                <li><p>{t("nav.restaurant")}</p></li>
+                <li><p>{t("nav.price")}</p></li>
+                <li><p>{t("nav.hotel")}</p></li>
             </ul>
         </nav>
     )
