28b5f5910f8512aa47ea50fe8bdc14aa2dafa711
Ola Rabcewicz <olar8871@gmail.com>
2022-04-02 17:16:01 +0200

    popupCalendar

--- src/components/routes/salaryTab/SalaryTab.js
+++ src/components/routes/salaryTab/SalaryTab.js
@@ -1,10 +1,18 @@
 import './SalaryTab.css';
-
+import React, { useEffect, useState } from 'react';
+import PopupCalendar from "../../popups/popupCalendar/PopupCalendar";
 const SalaryTab = () => {
-
+        const [isPopupCalendar, setIsPopupCalendar]=useState(false);
     return (
         <div >
-            SalaryTab
+            SalaryTab <br/>
+            SalaryTab <br/>
+            SalaryTab <br/>
+            <button onClick={()=>setIsPopupCalendar(true)}>KLIKNIJ PIZDO</button>
+            <PopupCalendar
+                open={isPopupCalendar}
+                close={()=>setIsPopupCalendar(false)}
+            />
         </div>
     )
 }
