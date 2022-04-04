dc883aeeebd6fa5972c0c842fea4b50f31c86817
Mateusz Wołek <sekwoja5@gmail.com>
2022-04-04 19:31:50 +0200

    Dokończenie szkieletu RoomWindow, zaryswoanie idei w css

--- src/components/routes/salaryTab/RoomWindow/RoomWindow.js
+++ src/components/routes/salaryTab/RoomWindow/RoomWindow.js
@@ -11,10 +11,35 @@ const RoomWindow = () => {
            <form>
             <div id="Parameters">
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <img id="bed"></img><li>Łóżko jednoosobowe</li>
+                    <img id="airCon"></img><li>Klimatyzacja</li>
+                    <img id="wiFi"></img><li>Bezpłatne Wi-Fi</li>
                 </ul>
+                <ul>
+                    <li>bezpłatny zestaw kosmetyków</li>
+                    <li>toaleta</li>
+                    <li>wanna lub prysznic</li>
+                    <li>ręczniki</li>
+                    <li>szafa</li>
+                    <li>TV</li>
+                    <li>lodówka</li>
+                    <li>telefon</li>
+                    <li>biurko</li>
+                    <li>suszarka do włosów</li>
+                    <li>balkon</li>
+                    <input type="checkbox">dostawka</input>
+                    <input type="checkbox">śniadanie</input>
+                </ul>
+            </div>
+            <div>
+                <select>
+                    <p>Liczba pokoi</p>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                </select>
+                <input type="submit" value="Rezerwuj"></input>
             </div>
            </form>
         </div>
