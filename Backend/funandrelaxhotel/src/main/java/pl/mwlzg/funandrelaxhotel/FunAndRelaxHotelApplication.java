b1b0dfbcd13eedcff5cffec9d63f330292ca509c
Mateusz Wołek <sekwoja5@gmail.com>
2022-04-15 23:03:36 +0200

    Rekonstrukcja drzewka projektowego, dodanie bazy danych w H2 i szkieletu backendu

---
+++ Backend/funandrelaxhotel/src/main/java/pl/mwlzg/funandrelaxhotel/FunAndRelaxHotelApplication.java
@@ -0,0 +1,13 @@
+package pl.mwlzg.funandrelaxhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunAndRelaxHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunAndRelaxHotelApplication.class, args);
+	}
+
+}
---
+++ Backend/funandrelaxhotel/src/test/java/pl/mwlzg/funandrelaxhotel/FunAndRelaxHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mwlzg.funandrelaxhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunAndRelaxHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
