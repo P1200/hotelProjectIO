13ceb4920c6c2571181a4b163e65bf1ff8384c73
Pulitus <kuba.pulik@gmail.com>
2022-04-24 22:30:41 +0200

    Psiota

--- Frontend/src/components/routes/spaTab/SpaTab.js
+++ Frontend/src/components/routes/spaTab/SpaTab.js
@@ -1,4 +1,5 @@
 import './SpaTab.css';
+import { BsPlayFill } from "react-icons/bs";
 
 const SpaTab = () => {
 
@@ -16,26 +17,43 @@ const SpaTab = () => {
             <div>
                 Cennik
                 <div>
-                    ceny
+                    <div>
+                        <p><BsPlayFill/> Zabieg oczyszczający</p>
+                        <p><BsPlayFill/> Zabieg regenerujący</p>
+                        <p><BsPlayFill/> Masaż twarzy</p>
+                        <p><BsPlayFill/> Masaż twarzy z maską algową</p>
+                        <p><BsPlayFill/> Peeling kawitacyjny</p>
+                        <p><BsPlayFill/> Peeling kawitacyjny</p>
+                        <p><BsPlayFill/> Pakiet zabiegów dla 2 osób</p>
+                    </div>
+                    <div>
+                        <p>99zł</p>
+                        <p>115zł</p>
+                        <p>60zł</p>
+                        <p>85zł</p>
+                        <p>150zł</p>
+                        <p>200zł</p>
+                        <p>400zł</p>
+
+                    </div>
                 </div>
             </div>
             <div>
                 Dostępne obiekty
                 <div>
-                    <div>zdjecia</div>
+                    <div id="pesiota"></div>
                     <p>Prywatne jacuzzi</p>
                 </div>
                 <div>
-                    <div>zdjecia</div>
+                    <div id="pesiota"></div>
                     <p>Basen wewnętrzny</p>
                 </div>
                 <div>
-                    <div>zdjecia</div>
-                    
+                    <div id="pesiota"></div>
                     <p>Pokój do masażu</p>
                 </div>
                 <div>
-                    <div>zdjecia</div>
+                    <div id="pesiota"></div>
                     <p>Pokój do masażu dla 2 osób z jacuzzi</p>
                 </div>
             </div>
