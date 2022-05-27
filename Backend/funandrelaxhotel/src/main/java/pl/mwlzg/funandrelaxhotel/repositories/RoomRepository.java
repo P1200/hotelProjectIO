4f9ccfe9627d9308c42af10c183b424d075759c0
Mateusz Wołek <sekwoja5@gmail.com>
2022-05-27 20:58:21 +0200

    Dodanie ścieżek do zdjęć, poprawa GET room

--- Backend/funandrelaxhotel/src/main/java/pl/mwlzg/funandrelaxhotel/repositories/RoomRepository.java
+++ Backend/funandrelaxhotel/src/main/java/pl/mwlzg/funandrelaxhotel/repositories/RoomRepository.java
@@ -6,6 +6,10 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import pl.mwlzg.funandrelaxhotel.sqltables.Room;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 
 @Repository
 public class RoomRepository {
@@ -13,12 +17,21 @@ public class RoomRepository {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    public Room getRoomById (int room_id){
+    public List<Room> getRoomById () {
 
-        return jdbcTemplate.queryForObject(
-                "SELECT * FROM room WHERE room_id= ?",
-                BeanPropertyRowMapper.newInstance(Room.class),
-                room_id
+        List<Room> roomList = jdbcTemplate.query(
+                "SELECT DISTINCT room.prise,room.kind,room.description,room_kind.path " +
+                        "FROM room INNER JOIN room_kind " +
+                        "ON room.kind=room_kind.kind",
+                BeanPropertyRowMapper.newInstance(Room.class)
         );
+        Path currentRelativePath = Paths.get("");
+        System.out.println("------------");
+        System.out.println("Path to the class: " + currentRelativePath.toAbsolutePath());
+        for (Room room : roomList){
+            room.setPath(currentRelativePath.toAbsolutePath() + "\\..\\Pictures\\" + room.getPath());
+            System.out.println(room.getPath());
+        }
+        return roomList;
     }
 }
